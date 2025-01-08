2b35cb8fb2917bded733b38bd7e764b6f13b631a
remigarcia31 <remi.garcia31pro@gmail.com>
2025-01-08 22:06:54 +0100

    dimension datetime

--- include/dbt/transform/dim_datetime.sql
+++ include/dbt/transform/dim_datetime.sql
@@ -21,7 +21,5 @@ SELECT
   EXTRACT(YEAR FROM date_part) AS year,
   EXTRACT(MONTH FROM date_part) AS month,
   EXTRACT(DAY FROM date_part) AS day,
-  EXTRACT(HOUR FROM date_part) AS hour,
-  EXTRACT(MINUTE FROM date_part) AS minute,
   EXTRACT(DAYOFWEEK FROM date_part) AS weekday
 FROM datetime_cte
