c71ba385605ac6da3ead5cb35579f093adc5d512
remigarcia31 <remi.garcia31pro@gmail.com>
2025-01-13 16:15:12 +0100

    error : missing [

--- include/dbt/models/transform/fact_invoices.sql
+++ include/dbt/models/transform/fact_invoices.sql
@@ -3,7 +3,7 @@ WITH fct_invoices_cte AS (
     SELECT
         TransactionID AS invoice_id,
         TransactionDate AS datetime_id,
-        {{ dbt_utils.generate_surrogate_key('ProductCategory', 'PricePerUnit']) }} as product_id,
+        {{ dbt_utils.generate_surrogate_key(['ProductCategory', 'PricePerUnit']) }} as product_id,
         {{ dbt_utils.generate_surrogate_key(['CustomerID', 'Country']) }} as customer_id,
         Quantity AS quantity,
         TotalAmount AS total
@@ -20,4 +20,4 @@ SELECT
 FROM fct_invoices_cte fi
 INNER JOIN {{ ref('dim_datetime') }} dt ON fi.datetime_id = dt.datetime_id
 INNER JOIN {{ ref('dim_product') }} dp ON fi.product_id = dp.product_id
-INNER JOIN {{ ref('dim_customer') }} dc ON fi.customer_id = dc.customer_id
+INNER JOIN {{ ref('dim_customers') }} dc ON fi.customer_id = dc.customer_id
